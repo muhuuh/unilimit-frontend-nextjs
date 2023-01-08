6c7492c64385a56ceccb79fba09499bb1e0b3d7d
arani <aranimontes@protonmail.com>
2023-01-08 17:40:29 +0100

    swap logic implemented

--- components/Connected/Ordering/OrderBoxSwapBot.js
+++ components/Connected/Ordering/OrderBoxSwapBot.js
@@ -1,36 +1,40 @@
 import React, { useState } from "react";
 //import { useUniswapV3 } from "../../lib/uniswapV3";
-import { Form, Input, Button, useMoralis } from "react-moralis";
+import { useMoralis } from "react-moralis";
 import { useUniswapV3 } from "../../../hooks/use-uniswap";
 
-const TokenSwap = () => {
+const OrderBoxSwapBot = () => {
   const [fromToken, setFromToken] = useState("");
   const [toToken, setToToken] = useState("");
   const [amount, setAmount] = useState(0);
   const [swapError, setSwapError] = useState(null);
   const [swapLoading, setSwapLoading] = useState(false);
   const uniswap = useUniswapV3();
-  const {
-    chainId: chainIdHex,
-    isWeb3Enabled,
-    isAuthenticated,
-    authenticate,
-    account,
-    web3,
-  } = useMoralis();
+  const { web3 } = useMoralis();
 
   const handleSwap = async () => {
     setSwapError(null);
     setSwapLoading(true);
 
+    if (!uniswap) {
+      setSwapError("Uniswap is not initialized");
+      setSwapLoading(false);
+      return;
+    }
+
     try {
       const fromTokenAddress = web3.utils.toChecksumAddress(fromToken);
       const toTokenAddress = web3.utils.toChecksumAddress(toToken);
       const amountInWei = web3.utils.toWei(amount.toString());
-      const txHash = await uniswap.swap(
+      const options = {
+        from: web3.eth.defaultAccount,
+        gasPrice: web3.utils.toWei("5", "gwei"),
+      };
+      const txHash = await uniswap.Trade.swapExactTokensForTokens(
+        amountInWei,
+        options,
         fromTokenAddress,
-        toTokenAddress,
-        amountInWei
+        toTokenAddress
       );
 
       console.log(`Token swap transaction hash: ${txHash}`);
@@ -42,32 +46,46 @@ const TokenSwap = () => {
   };
 
   return (
-    <Form>
+    <div>
       <h1>Token Swap</h1>
-      <Input
-        label="From Token"
-        type="text"
-        value={fromToken}
-        onChange={(event) => setFromToken(event.target.value)}
-      />
-      <Input
-        label="To Token"
-        type="text"
-        value={toToken}
-        onChange={(event) => setToToken(event.target.value)}
-      />
-      <Input
-        label="Amount"
-        type="number"
-        value={amount}
-        onChange={(event) => setAmount(event.target.value)}
-      />
-      <Button onClick={handleSwap} disabled={swapLoading}>
-        Swap
-      </Button>
-      {swapError && <p style={{ color: "red" }}>{swapError}</p>}
-    </Form>
+      {swapError && <p className="error">{swapError}</p>}
+      <form onSubmit={handleSwap}>
+        <label htmlFor="fromToken">
+          From Token:
+          <input
+            type="text"
+            id="fromToken"
+            value={fromToken}
+            onChange={(event) => setFromToken(event.target.value)}
+          />
+        </label>
+        <br />
+        <label htmlFor="toToken">
+          To Token:
+          <input
+            type="text"
+            id="toToken"
+            value={toToken}
+            onChange={(event) => setToToken(event.target.value)}
+          />
+        </label>
+        <br />
+        <label htmlFor="amount">
+          Amount:
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={(event) => setAmount(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit" disabled={swapLoading}>
+          Swap
+        </button>
+      </form>
+    </div>
   );
 };
 
-export default TokenSwap;
+export default OrderBoxSwapBot;
--- hooks/use-uniswap.js
+++ hooks/use-uniswap.js
@@ -1,16 +1,22 @@
 import { useEffect, useState } from "react";
 import UniswapV3 from "@uniswap/v3-sdk";
+import { useMoralis } from "react-moralis";
 
 export const useUniswapV3 = () => {
   const [uniswap, setUniswap] = useState(null);
+  const { web3 } = useMoralis();
 
   useEffect(() => {
+    if (!web3 || !web3.currentProvider) {
+      return;
+    }
+
     (async () => {
-      const provider = new UniswapV3.Provider();
+      const provider = new UniswapV3.Provider(web3.currentProvider);
       const instance = new UniswapV3(provider);
       setUniswap(instance);
     })();
-  }, []);
+  }, [web3]);
 
   return uniswap;
 };
